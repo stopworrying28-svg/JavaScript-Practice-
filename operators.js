70c48cc2ced6bc4a9d42ba502a21de918b328dc6
Zunaira <stopworrying28@gmail.com>
2026-08-16 16:19:46 +0500

    Operators of javascript added

---
+++ operators.js
@@ -0,0 +1,30 @@
+console.log("Operators in JavaScript");
+let a = 15;
+let b = 5;
+
+console.log("Arithmetic Operators");
+console.log("Num1 + Num2 =", a + b);
+console.log("Num1 - Num2 =", a - b);
+console.log("Num1 * Num2 =", a * b);
+console.log("Num1 / Num2 =", a / b);
+console.log("Num1 % Num2 =", a % b);
+console.log("++Num1 =", ++a);
+console.log("Num2++ =", b++);
+console.log("--Num1 =", --a);
+console.log("Num2-- =", b--);
+console.log("Num1 ** Num2 =", a ** b);
+console.log("\n");
+
+console.log("Comparison Operators")
+console.log("Num1 == Num2 =", a == b);
+console.log("Num1 === Num2 =", a === b);
+console.log("Num1 >= Num2 =", a >= b);
+console.log("Num1 <= Num2 =", a <= b);
+console.log("Num1 != Num2 =", a != b);
+console.log("Num1 !== Num2 =", a !== b);
+console.log("\n");
+
+console.log("Logical Operators");
+console.log("Num1 == Num2 && Num1 >= Num2 =", a == b && a >= b );
+console.log("Num1 == Num2 || Num1 >= Num2 =", a == b || a >= b );
+console.log("!Num1 != Num2 =", !(a != b));
