dbd6ca419377b51d371cc9d88c85b45a817d3b29
Zunaira <stopworrying28@gmail.com>
2026-08-17 12:08:59 +0500

    Object added

---
+++ object.js
@@ -0,0 +1,14 @@
+console.log("Object-Practice")
+let Student={
+    name: "Bunny",
+    id: 4252352,
+    age: 20,
+    grade: 10,
+    isStudent: true
+}
+console.log("***** Student Information *****");
+console.log("Student Name:", Student.name);
+console.log("Student ID:", Student.id);
+console.log("Student Age:", Student.age);
+console.log("Student Grade:", Student.grade);
+console.log("Student Status:", Student.isStudent);
