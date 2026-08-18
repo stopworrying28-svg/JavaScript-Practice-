a7b419bb6deffdd23d0613bb4be01f710405e952
Zunaira <stopworrying28@gmail.com>
2026-08-18 22:07:02 +0500

    Table added

---
+++ Table.js
@@ -0,0 +1,13 @@
+console.log("Table");
+//try adding different numbers
+let num = Number(prompt("Enter the number: "));
+console.log(num, "* 1 =", num * 1);
+console.log(num, "* 2 =", num * 2);
+console.log(num, "* 3 =", num * 3);
+console.log(num, "* 4 =", num * 4);
+console.log(num, "* 5 =", num * 5);
+console.log(num, "* 6 =", num * 6);
+console.log(num, "* 7 =", num * 7);
+console.log(num, "* 8 =", num * 8);
+console.log(num, "* 9 =", num * 9);
+console.log(num, "* 10 =", num * 10);
